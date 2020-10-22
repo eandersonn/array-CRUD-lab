19cbb8b9a8fbf5f892f500d14f4e6810367d6492
Erin Anderson <e.anderson1226@gmail.com>
2020-10-22 09:50:57 -0400

    commit tree 00d32bc705dc0a4f292c30306a851789e9179373

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["erin", "2000", "capricorn","blue"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+   array = ["erin", "2000", "capricorn", "blue"]
+   array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["wow", "2000", "capricorn", "blue"]
+  array.unshift
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["erin", "2000", "capricorn", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow", "2000", "capricorn", "blue"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["wow", "2000", "am", "blue"]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow", "2000", "capricorn", "blue"]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["wow", "2000", "capricorn", "arrays!"]
+  array[3]
 end
